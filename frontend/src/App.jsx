970db0baaf84fb5850449492da3239895dab5dd6
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2025-04-24 12:43:01 +0100

    created login compo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Image from './assets/Image.jpeg';
+import Login from './pages/Login';
+
+// import Register from './pages/Register';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+<div className="text-white h-[100vh] flex justify-center items-center bg-cover" style={{ backgroundImage: `url(${Image})` }}
+    >        {/* <Routes>
+          <Route path='login' element={<Login/>}/>
+          <Route path='register' element={<Register/>}/>
+        </Routes> */}
+        <Login/>
+    </div>
+  );
 }
